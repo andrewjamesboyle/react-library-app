c09743b25ab17b2553ef12a91347b8c9ed7d41f1
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-26 16:21:07 -0700

    pathing is set up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import BookList from './components/book/BookList';
 import './App.css';
 import BookDetail from './components/book/BookDetail';
 import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <Switch>
-      <Route exact path="/" >
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/books" >
         <main className="container">
           <h1>Library Catalog</h1>
           <BookList />
--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,6 +1,6 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function BookDetail() {
   const { id } = useParams();
@@ -15,7 +15,12 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Link key={book} to={`/books`}>Return to Books</Link>
+      <Book book={book} showDetail />;
+    </>
+  );
 }
 
 export default BookDetail;
