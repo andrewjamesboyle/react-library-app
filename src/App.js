1038581a9c1bdbac222f77447760d7aeaa80ef5b
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-26 15:19:33 -0700

    hardcode book id gets book details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import BookDetail from './components/book/BookDetail';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/" component={BookList} />
+        <Route path="/books/:id" component={BookDetail} />
+      </Switch>
     </main>
   );
 }
