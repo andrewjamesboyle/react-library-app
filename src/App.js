971ee0341d8e75384bda05804e710f138135fcb0
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-26 15:35:08 -0700

    books list is clickable

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <Switch>
-        <Route exact path="/" component={BookList} />
-        <Route path="/books/:id" component={BookDetail} />
-      </Switch>
-    </main>
+    <Switch>
+      <Route exact path="/" >
+        <main className="container">
+          <h1>Library Catalog</h1>
+          <BookList />
+        </main>
+      </Route>
+
+      <Route path="/books/:id" >
+        <BookDetail />
+      </Route>
+    </Switch>
   );
 }
 
